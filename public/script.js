bc21408b2674182a79aba4ce8d284c992852f90a
Your Name <you@example.com>
2018-05-16 19:12:20 +0400

    new

--- public/script.js
+++ public/script.js
@@ -1,4 +1,5 @@
-var matrix = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+
+var matrix = [[0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -6,11 +7,34 @@ var matrix = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];
-function preload() {
-    img = loadImage('img//laDefense.jpg');
-  }
-  
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];
+var side = 32;
+
+
+
+// function preload() {
+//     img = loadImage('img/gui/truck/default.png');
+//   }
+for (var y = 0; y < matrix.length; y++) {
+    for (var x = 0; x < matrix[y].length; x++) {
+        if (matrix[y][x] == 1) {
+
+            var playerx = x;
+            var playery = y;
+        }
+    }
+}
 function setup() {
     createCanvas(matrix[0].length * side, matrix.length * side);
+    //image(img, 0, 0);
+    background('#acacac');
+
+}
+function draw() {
+    rect(playerx * side, playery * side, side, side);
+
+    if (keyIsDown(40 || 83)) {
+        playery--;
+    }
+
 }
